939344e06c767c8af29c30a63b83de611220e1af
MickyOr <mickyor97@gmail.com>
2020-10-14 13:55:24 -0400

    helpers getDia y getFecha ya no estan hardcodeados; agregado helper traducirDia

--- app/helpers.php
+++ app/helpers.php
@@ -3,11 +3,25 @@
 // devuelve el dia actual
 function getDia()
 {
-    return 'MARTES';
+    return strtoupper( traducirDia( date("l") ) );
+    //return 'MARTES';
 }
 
 // devuelve la fecha actual
 function getFecha()
 {
-    return "12/10/2020";
+    return date("d/m/Y");
+    //return "12/10/2020";
+}
+
+// traduce el dia de semana de ingles a español
+// si se puede, cambiar a __()
+function traducirDia($dia) {
+    if ($dia == "Monday") return "Lunes";
+    if ($dia == "Tuesday") return "Martes";
+    if ($dia == "Wednesday") return "Miercoles";
+    if ($dia == "Thursday") return "Jueves";
+    if ($dia == "Friday") return "Viernes";
+    throw new Exception("Se esperaba un dia de la semana en ingles");
+    
 }
