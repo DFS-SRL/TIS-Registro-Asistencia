de9bd3d3e8512be0bc89377b4da69cff142da20e
MickyOr <mickyor97@gmail.com>
2020-10-19 12:26:49 -0400

    obtener informacion de grupo

---
+++ app/Http/Controllers/GrupoController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Grupo;
+use App\HorarioClase;
+use App\Usuario;
+use App\UsuarioTieneRol;
+use App\Rol;
+use Illuminate\Http\Request;
+
+class GrupoController extends Controller
+{
+    public function mostrarInformacion(Grupo $grupo) {
+        // Obtener los horarios correspondientes a la materia
+        $horarios = HorarioClase::where('grupo_id', '=', $grupo->id)
+                                    ->orderBy('dia', 'ASC')
+                                    ->orderBy('hora_inicio', 'ASC')
+                                    ->get();
+
+        // IMPORTANTE
+        // Se asume que cada grupo tiene asignado maximo un docente y un auxiliar
+
+        // Obtiene el docente asignado al grupo
+        $docente = Usuario::join('Usuario_tiene_rol', 'Usuario_tiene_rol.usuario_codSis', '=', 'codSis')
+                            -> join('Rol', 'Rol.id', '=', 'Usuario_tiene_rol.rol_id') 
+                            -> where('Rol.id', '=', 3)
+                            -> join('Horario_clase', 'Horario_clase.asignado_codSis', '=', 'codSis')
+                            -> where('Horario_clase.grupo_id', '=', $grupo->id)
+                            -> select('Usuario.codSis', 'Usuario.nombre')
+                            -> get() -> first();
+        
+        // Obtiene el auxiliar asignado al grupo
+        $auxiliar = Usuario::join('Usuario_tiene_rol', 'Usuario_tiene_rol.usuario_codSis', '=', 'codSis')
+                            -> join('Rol', 'Rol.id', '=', 'Usuario_tiene_rol.rol_id') 
+                            -> where('Rol.id', '=', 2)
+                            -> join('Horario_clase', 'Horario_clase.asignado_codSis', '=', 'codSis')
+                            -> where('Horario_clase.grupo_id', '=', $grupo->id)
+                            -> select('Usuario.codSis', 'Usuario.nombre')
+                            -> get() -> first();
+
+        return [$horarios, $docente, $auxiliar];
+
+        /* // Obtener auxiliar asignado a la materia
+        $auxiliar = Usuario::with('horarioclase') 
+                                ->where('grupo_id', '=', $grupo->id)
+                                ->where('rol_id', '=', 3)
+                                ->get();
+
+        return "asdfas";
+        $datos = [
+            "horarios" => $horarios,
+            "docente" => $docente,
+            "auxiliar" => $auxiliar
+        ];
+        return $datos; */
+    }
+}
