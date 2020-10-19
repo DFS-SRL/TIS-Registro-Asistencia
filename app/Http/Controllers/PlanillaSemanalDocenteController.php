9f861c6ff8b6249411eeb546710b3099f7b445b8
javier0097 <javier0097@hotmail.com>
2020-10-19 18:14:12 -0400

    vista de la planilla semanal de docentes

--- app/Http/Controllers/PlanillaSemanalDocenteController.php
+++ app/Http/Controllers/PlanillaSemanalDocenteController.php
@@ -20,19 +20,16 @@ class PlanillaSemanalDocenteController extends Controller
         $departamentos = Unidad::all();
 
         foreach ($departamentos as $dpto) {
-            $horarios = HorarioClase::where('asignado_codSis', '=', $user->codSis) 
-                                        -> where('rol_id', '=', 3) 
-                                        -> where('unidad_id', '=', $dpto->id)
-                                        -> orderBy('dia', 'ASC')
-                                        -> get();
+            $horarios = HorarioClase::where('asignado_codSis', '=', $user->codSis)-> where('rol_id', '=', 3)-> where('unidad_id', '=', $dpto->id)-> orderBy('dia', 'ASC')-> get();
             if (!$horarios->isEmpty())
                 array_push($horariosPorDpto, $horarios);
         }
 
+        //return $horariosPorDpto;
         $fechasDeSemana = getFechasDeSemanaActual();
 
         // devolver vista de planillas semanales
-        return view('planillas.semanalDocentePrueba', [
+        return view('planillas.semanalDocente', [
             'fechaInicio' => $fechasDeSemana["LUNES"],
             'fechaFinal' => $fechasDeSemana["VIERNES"],
             'fechasDeSemana' => $fechasDeSemana,
