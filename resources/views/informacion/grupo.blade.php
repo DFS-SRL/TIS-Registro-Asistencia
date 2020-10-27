a1becad7b6fa6db4037f85a33aa61551f04c5b6c
MickyOr <mickyor97@gmail.com>
2020-10-27 05:41:37 -0400

    estilos mas consistentes en vista de materia

--- resources/views/informacion/grupo.blade.php
+++ resources/views/informacion/grupo.blade.php
@@ -9,74 +9,68 @@
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/estiloGeneral.css">
-    <style>
-        table,
-        th,
-        td {
-            border: 1px solid black;
-        }
-
-    </style>
 </head>
 
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-8">
-                <h2 class="textoBlanco">{{ $grupo->unidad->facultad }}</h4>
-                <h2 class="textoBlanco">{{ $grupo->unidad->nombre }}</h1>
-                <h4 class="textoBlanco">{{ $grupo->materia->nombre }}</h4>
-                <h4 class="textoBlanco">{{ $grupo->nombre }}</h4>
+    <div class="mx-3 my-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <h2 class="textoBlanco">{{ $grupo->unidad->facultad }}</h4>
+                    <h2 class="textoBlanco">{{ $grupo->unidad->nombre }}</h1>
+                    <h4 class="textoBlanco">{{ $grupo->materia->nombre }}</h4>
+                    <h4 class="textoBlanco">{{ $grupo->nombre }}</h4>
+                </div>
+                <div class="col-4">
+                    <button class="btn btn-primary">EDITAR</button>
+                </div>
             </div>
-            <div class="col-4">
-                <button class="btn btn-primary">EDITAR</button>
-            </div>
-        </div>
-        <table class="table table-bordered">
-            <thead class="thead-dark">
-                <tr>
-                    <th class="textoBlanco border border-dark" scope="col">HORARIO</th>
-                    <th class="textoBlanco border border-dark" scope="col">CARGO</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse($horarios as $key => $horario)
+            <table class="table table-bordered">
+                <thead class="thead-dark">
                     <tr>
-                        <td class="border border-dark">{{ $horario->dia }} {{ $horario->hora_inicio }} - {{ $horario->hora_fin }}</td>
-                        <td class="border border-dark">
-                            @if ($docente != null && $horario->asignado_codSis === $docente->codSis)
-                                DOCENCIA
-                            @elseif ($auxiliar != null && $horario->asignado_codSis === $auxiliar->codSis)
-                                AUXILIATURA
-                            @else
-                                HORARIO NO ASIGNADO
-                            @endif
-                        </td>
+                        <th class="textoBlanco border border-dark" scope="col">HORARIO</th>
+                        <th class="textoBlanco border border-dark" scope="col">CARGO</th>
                     </tr>
-                @empty
-                    <h5 class="textoBlanco">NO HAY HORARIOS</h5>
-                @endforelse
-            </tbody>
-        </table>
-        <div class="row">
-            <div class="col-12">
-                @if ($horarios != null && $horarios->where('rol_id', '=', 3)->count() > 0)
-                    @if ($docente != null)
-                        <h4 class="textoBlanco">Docente: {{$docente->nombre}}</h4> 
-                        <h4 class="textoBlanco">Carga horaria docente: {{$cargaHorariaDocente}}</h4>
-                    @else
-                        <h4 class="textoBlanco">Docente no asignado</h4>
+                </thead>
+                <tbody>
+                    @forelse($horarios as $key => $horario)
+                        <tr>
+                            <td class="border border-dark">{{ $horario->dia }} {{ $horario->hora_inicio }} - {{ $horario->hora_fin }}</td>
+                            <td class="border border-dark">
+                                @if ($docente != null && $horario->asignado_codSis === $docente->codSis)
+                                    DOCENCIA
+                                @elseif ($auxiliar != null && $horario->asignado_codSis === $auxiliar->codSis)
+                                    AUXILIATURA
+                                @else
+                                    HORARIO NO ASIGNADO
+                                @endif
+                            </td>
+                        </tr>
+                    @empty
+                        <h5 class="textoBlanco">NO HAY HORARIOS</h5>
+                    @endforelse
+                </tbody>
+            </table>
+            <div class="row">
+                <div class="col-12">
+                    @if ($horarios != null && $horarios->where('rol_id', '=', 3)->count() > 0)
+                        @if ($docente != null)
+                            <h4 class="textoBlanco">Docente: {{$docente->nombre}}</h4> 
+                            <h4 class="textoBlanco">Carga horaria docente: {{$cargaHorariaDocente}}</h4>
+                        @else
+                            <h4 class="textoBlanco">Docente no asignado</h4>
+                        @endif
                     @endif
-                @endif
-                @if ($horarios != null && $horarios->where('rol_id', '<=', 2)->count() > 0)
-                    @if ($auxiliar != null)
-                        <h4 class="textoBlanco">Auxiliar: {{$auxiliar->nombre}}</h4>
-                        <h4 class="textoBlanco">Carga horaria auxilliar: {{$cargaHorariaAuxiliar}}</h4>
-                    @else
-                        <h4 class="textoBlanco">Auxiliar no asignado</h4>
+                    @if ($horarios != null && $horarios->where('rol_id', '<=', 2)->count() > 0)
+                        @if ($auxiliar != null)
+                            <h4 class="textoBlanco">Auxiliar: {{$auxiliar->nombre}}</h4>
+                            <h4 class="textoBlanco">Carga horaria auxilliar: {{$cargaHorariaAuxiliar}}</h4>
+                        @else
+                            <h4 class="textoBlanco">Auxiliar no asignado</h4>
+                        @endif
                     @endif
-                @endif
+                </div>
             </div>
         </div>
     </div>
--- resources/views/informacion/materia.blade.php
+++ resources/views/informacion/materia.blade.php
@@ -5,41 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Auxiliar laboratorio</title>
+    <title>Materia</title>
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/estiloGeneral.css">
-    <style>
-        table,
-        th,
-        td {
-            border: 1px solid black;
-        }
-
-    </style>
 </head>
 
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-8">
-                <p>{{ $materia->unidad->facultad }}</p>
-                <p>{{ $materia->unidad->nombre }}</p>
-                <p>{{ $materia->nombre }}</p>
+    <div class="mx-3 my-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <h2 class="textoBlanco">{{ $materia->unidad->facultad }}</h4>
+                    <h2 class="textoBlanco">{{ $materia->unidad->nombre }}</h4>
+                    <h4 class="textoBlanco">{{ $materia->nombre }}</h4>
+                </div>
+                <div class="col-4">
+                    <h4 class="textoBlanco">Codigo: {{$materia->id}}</h4>
+                </div>
             </div>
-            <div class="col-4">
-                <p>Codigo: {{$materia->id}}</p>
-            </div>
+            <ul class="list-group">
+                @forelse ($grupos as $grupo)
+                    <li class="list-group-item lista"><a href="/grupo/{{$grupo->id}}">{{$grupo->nombre}}</a></li>
+                @empty
+                    <h4 class="textoBlanco">No hay grupos</h4>
+                @endforelse
+            </ul>
+            <button class="btn btn-success">AÑADIR GRUPO</button>
         </div>
-        <ul class="list-group">
-            @forelse ($grupos as $grupo)
-                <li class="list-group-item"><a href="/grupo/{{$grupo->id}}">{{$grupo->nombre}}</a></li>
-            @empty
-                <p>No hay grupos</p>
-            @endforelse
-        </ul>
-        <button class="btn btn-success">AÑADIR GRUPO</button>
     </div>
 </body>
 <!-- jQuery and JS bundle w/ Popper.js -->
