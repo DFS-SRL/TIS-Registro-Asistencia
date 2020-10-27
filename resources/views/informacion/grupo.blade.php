aefedec0b36e62e0a448ce982ebca32888daa215
MickyOr <mickyor97@gmail.com>
2020-10-27 05:36:05 -0400

    estilos mas consistentes en vista de grupo

--- resources/views/informacion/grupo.blade.php
+++ resources/views/informacion/grupo.blade.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Auxiliar laboratorio</title>
+    <title>Grupo</title>
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/estiloGeneral.css">
     <style>
         table,
         th,
@@ -23,27 +24,27 @@
     <div class="container">
         <div class="row">
             <div class="col-8">
-                <p>{{ $grupo->unidad->facultad }}</p>
-                <p>{{ $grupo->unidad->nombre }}</p>
-                <p>{{ $grupo->materia->nombre }}</p>
-                <p>{{ $grupo->nombre }}</p>
+                <h2 class="textoBlanco">{{ $grupo->unidad->facultad }}</h4>
+                <h2 class="textoBlanco">{{ $grupo->unidad->nombre }}</h1>
+                <h4 class="textoBlanco">{{ $grupo->materia->nombre }}</h4>
+                <h4 class="textoBlanco">{{ $grupo->nombre }}</h4>
             </div>
             <div class="col-4">
                 <button class="btn btn-primary">EDITAR</button>
             </div>
         </div>
-        <table class="table">
+        <table class="table table-bordered">
             <thead class="thead-dark">
                 <tr>
-                    <th scope="col">HORARIO</th>
-                    <th scope="col">CARGO</th>
+                    <th class="textoBlanco border border-dark" scope="col">HORARIO</th>
+                    <th class="textoBlanco border border-dark" scope="col">CARGO</th>
                 </tr>
             </thead>
             <tbody>
                 @forelse($horarios as $key => $horario)
                     <tr>
-                        <td>{{ $horario->dia }} {{ $horario->hora_inicio }} - {{ $horario->hora_fin }}</td>
-                        <td>
+                        <td class="border border-dark">{{ $horario->dia }} {{ $horario->hora_inicio }} - {{ $horario->hora_fin }}</td>
+                        <td class="border border-dark">
                             @if ($docente != null && $horario->asignado_codSis === $docente->codSis)
                                 DOCENCIA
                             @elseif ($auxiliar != null && $horario->asignado_codSis === $auxiliar->codSis)
@@ -54,7 +55,7 @@
                         </td>
                     </tr>
                 @empty
-                    <p>NO HAY HORARIOS</p>
+                    <h5 class="textoBlanco">NO HAY HORARIOS</h5>
                 @endforelse
             </tbody>
         </table>
@@ -62,18 +63,18 @@
             <div class="col-12">
                 @if ($horarios != null && $horarios->where('rol_id', '=', 3)->count() > 0)
                     @if ($docente != null)
-                        <p>Docente: {{$docente->nombre}}</p>                    
-                        <p>Carga horaria docente: {{$cargaHorariaDocente}}</p>
+                        <h4 class="textoBlanco">Docente: {{$docente->nombre}}</h4> 
+                        <h4 class="textoBlanco">Carga horaria docente: {{$cargaHorariaDocente}}</h4>
                     @else
-                        <p>Docente no asignado</p>
+                        <h4 class="textoBlanco">Docente no asignado</h4>
                     @endif
                 @endif
                 @if ($horarios != null && $horarios->where('rol_id', '<=', 2)->count() > 0)
                     @if ($auxiliar != null)
-                        <p>Auxiliar: {{$auxiliar->nombre}}</p>
-                        <p>Carga horaria auxilliar: {{$cargaHorariaAuxiliar}}</p>
+                        <h4 class="textoBlanco">Auxiliar: {{$auxiliar->nombre}}</h4>
+                        <h4 class="textoBlanco">Carga horaria auxilliar: {{$cargaHorariaAuxiliar}}</h4>
                     @else
-                        <p>Auxiliar no asignado</p>
+                        <h4 class="textoBlanco">Auxiliar no asignado</h4>
                     @endif
                 @endif
             </div>
