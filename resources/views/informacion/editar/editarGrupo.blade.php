619fe7906338a71abff522eaa780269a1da7f20b
OscarMiddagh <oscardanie98@gmail.com>
2020-11-03 09:09:52 -0400

    añadir horario parte 1

--- resources/views/informacion/editar/editarGrupo.blade.php
+++ resources/views/informacion/editar/editarGrupo.blade.php
@@ -47,7 +47,7 @@
                         <th class="textoBlanco border border-dark" scope="col">OPCIONES</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody id = "cuerpo-tabla">
                     @forelse($horarios as $key => $horario)
                         <tr>
                             <td class="border border-dark">{{ $horario->dia }} {{ $horario->hora_inicio }} - {{ $horario->hora_fin }}</td>
@@ -71,7 +71,7 @@
                 </tbody>
             </table>
             
-            <button class="btn boton" id="añadirHorario">AÑADIR HORARIO</button>  
+            <button class="btn boton" id="añadirHorario" onclick="añadirHorario();">AÑADIR HORARIO</button>  
             <div class="row rounded-lg " id="personalAcademico">
                 <div class="col-12" >
                     @if ($horarios != null && $horarios->where('rol_id', '=', 3)->count() > 0)
@@ -86,10 +86,11 @@
                     @if ($horarios != null && $horarios->where('rol_id', '<=', 2)->count() > 0)
                         @if ($auxiliar != null)
                             <h4>Auxiliar: {{$auxiliar->nombre}}<input width="30rem" height="30rem" type="image" name="botonEliminar" src="/icons/eliminar.png" alt="Eliminar"></h4>
-                            <h4>Carga horaria auxilliar: {{$cargaHorariaAuxiliar}}</h4>
+                            <h4>Carga horaria auxilliar: {{$cargaHorariaAuxiliar}} </h4>
                             
                         @else
-                            <h4 >Auxiliar: <button class="btn boton" id="asignarAuxiliar">ASIGNAR AUXILIAR</button></h4> 
+                            <h4 >Auxiliar: <button class="btn boton" id="asignarAuxiliar">ASIGNAR AUXILIAR</button> </h4> <input type="text" id="name" name="name" required
+                            minlength="4" maxlength="8" size="10">
                         @endif
                     @endif
                 </div>
@@ -98,10 +99,49 @@
         </div>
     </div>
 </body>
+
 <script>
+    let numColumnas = document.getElementById('cuerpo-tabla').rows.length;
     function vistaGrupo(id){
-    location.href="/grupo/"+id;
-}
+        location.href="/grupo/"+id;
+    }
+    function añadirHorario(){
+        let cuerpoTabla = document.getElementById("cuerpo-tabla");
+        cuerpoTabla.innerHTML += `<tr id ="`+numColumnas+`">
+                                    <td class="border border-dark">
+                                        <select>
+                                            <option value="LUNES">LUNES</option>
+                                            <option value="MARTES">MARTES</option>
+                                            <option value="MIERCOLES">MIERCOLES</option>
+                                            <option value="JUEVES">JUEVES</option>
+                                            <option value="VIERNES">VIERNES</option>
+                                            <option value="SABADO">SABADO</option>
+                                        </select>
+                                        <input type="time" name="" id="">
+                                        <input type="time" name="" id="" disabled>
+                                        <input type="number" name="" id="" min="1" max="12" value="1">
+                                    </td>
+                                    <td class="border border-dark">
+                                        <select>
+                                            <option value="DOCENCIA">DOCENCIA</option>
+                                            <option value="AUXILIATURA">AUXILIATURA</option>
+                                        </select>
+                                    </td>
+                                    <td class="border border-dark">
+                                        <input width="30rem" height="30rem" type="image" name="botonAceptar" src="/icons/aceptar.png" alt="Aceptar" onclick="aceptarFila();">
+                                        <input width="30rem" height="30rem" type="image" name="botonCancelar" src="/icons/cancelar.png" alt="Cancelar" onclick = "cancelarFila();">
+                                    </td>
+                                </tr>`;
+    }
+    function aceptarFila(){
+        // numColumnas++;
+    }
+    function cancelarFila(){
+
+        fila = document.getElementById(numColumnas);
+        padre = fila.parentNode;
+        padre.removeChild(fila);
+    }
 </script>
 <!-- jQuery and JS bundle w/ Popper.js -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
