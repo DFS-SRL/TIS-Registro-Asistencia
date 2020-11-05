cd0a5d9e87d7fdfc178d4cda751a51cde77780a7
pacha2880 <salguero_ro@hotmail.com>
2020-11-05 18:14:09 -0400

    tareas 1.7.c y 1.8.c
    post para recuperar datos y verificar si es docente o auxiliar

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::get('/jefesDept', 'ProvController\Menu@jefesDept');
 Route::get('/departamentos', 'ProvController\Menu@departamentos');
 
 Route::get('/grupo/{grupo}/editar', 'GrupoController@editarInformacion');
+Route::post('/grupo/{grupo}/editar/esDocente', 'GrupoController@esDocente')->name('grupo.editar.esDocente');
 Route::get('/grupo/{grupo}', 'GrupoController@mostrarInformacion');
 Route::patch('/grupo/asignar/docente', 'GrupoController@asignarDocente')->name('grupo.asignar.docente');
 Route::patch('/grupo/asignar/auxDoc', 'GrupoController@asignarAuxDoc')->name('grupo.asignar.auxDoc');
