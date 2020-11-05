3666e6e8008b45e4d14956c60be86f264763cb3f
javier0097 <javier0097@hotmail.com>
2020-11-05 06:39:24 -0400

    commit para poder hacer pull

--- resources/views/informacion/editar/editarGrupo.blade.php
+++ resources/views/informacion/editar/editarGrupo.blade.php
@@ -91,7 +91,7 @@
                         @else
                             <h4 >Docente: <button class="btn boton" id="asignarDocente" onclick="botonAsignar('asignarDocente','botonBuscador1','buscador1','cancelar1','msgObsDocente',true)">ASIGNAR DOCENTE</button>
                             <form class="form-inline my-2 my-lg-0 d-inline" onsubmit="return validarBusquedaAsignar('buscador1','msgObsDocente')">
-                                <input id = "buscador1" class="oculto" type="search" placeholder="codSis docente" aria-label="Search">
+                                <input id = "buscador1" class="oculto" type="search" placeholder="codSis docente" aria-label="Search" name="buscador">
                                 <button id = "botonBuscador1" class="btn boton my-2 my-sm-0 oculto" type="submit">
                                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
