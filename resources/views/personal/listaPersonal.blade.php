ef47def0bf26e41f72cea300c3927c6fa0c592a7
pacha2880 <salguero_ro@hotmail.com>
2020-11-13 16:04:09 -0400

    vista trucha de personal

--- resources/views/personal/listaPersonal.blade.php
+++ resources/views/personal/listaPersonal.blade.php
@@ -1,1 +1,36 @@
-@include('layouts.flash-message')
+<h1>{{ $unidad->nombre }}</h1>
+<h3>todos</h3>
+<ul>
+    @forelse ($todos as $usuario)
+        <li>{{ $usuario->nombre }} {{ $usuario->codSis }} roles: @foreach ($usuario->roles as $rol) {{$rol->rol_id}}, @endforeach</li>
+    @empty
+        <p>no hay personal</p>
+    @endforelse
+</ul>
+<br>
+<h3>docentes</h3>
+<ul>
+    @forelse ($docentes as $usuario)
+        <li>{{ $usuario->nombre }} {{ $usuario->codSis }}</li>
+    @empty
+        <p>no hay docentes</p>
+    @endforelse
+</ul>
+<br>
+<h3>auxiliares de docencia</h3>
+<ul>
+    @forelse ($auxiliaresDoc as $usuario)
+        <li>{{ $usuario->nombre }} {{ $usuario->codSis }}</li>
+    @empty
+        <p>no hay auxiliares de docencia</p>
+    @endforelse
+</ul>
+<br>
+<h3>auxiliares de laboratorio</h3>
+<ul>
+    @forelse ($auxiliaresLabo as $usuario)
+        <li>{{ $usuario->nombre }} {{ $usuario->codSis }}</li>
+    @empty
+        <p>no hay auxiliares de laboratorio</p>
+    @endforelse
+</ul>
