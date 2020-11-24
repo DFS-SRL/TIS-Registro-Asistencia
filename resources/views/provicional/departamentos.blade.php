062153f4e29393972f1ce81c30489ebd930178cd
Oscar Daniel L <oscardanie98@gmail.com>
2020-11-24 08:06:01 -0400

    ver personal academico

--- resources/views/provicional/departamentos.blade.php
+++ resources/views/provicional/departamentos.blade.php
@@ -7,11 +7,11 @@
 <table>
     @forelse ($departamentos as $departamento)
     <tr>
-        <td class="col-3">{{$departamento->facultad}} - {{$departamento->nombre}}</td>
-        <td class="col-2">
+        <td class="col-2">{{$departamento->facultad}} - {{$departamento->nombre}}</td>
+        <td class="col-1">
         <a href="parteMensual/docentes/{{$departamento->id}}/2020-10-27">Parte Docentes</a>
         </td>
-        <td class="col-2">
+        <td class="col-1">
             <a href="parteMensual/auxiliares/{{$departamento->id}}/2020-10-27">Parte Auxiliares</a>
         </td>
         <td class="col-2">
@@ -22,6 +22,9 @@
         </td>        
         <td class="col-2">
             <a href="{{ route('informes', $departamento->id )}}">Informes</a>
+        </td>
+        <td class="col-2">
+            <a href="{{ route('informacion.personalAcademico', $departamento->id )}}">Personal Academico</a>
         </td>
     </tr>
     @empty
